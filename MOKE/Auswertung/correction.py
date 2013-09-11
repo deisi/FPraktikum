933661582e45da0cbe28087f7d5ca6f90d5f071b
deisi <mdeiseroth88@googlemail.de>
2013-09-11 20:39:30 +0200

    first commit

--- MOKE/Auswertung/correction.py
+++ MOKE/Auswertung/correction.py
@@ -5,7 +5,23 @@
 # 
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 from numpy import *
-import matplotlib.pyplot as plt
-import pylab as py
-from scipy.optimize import minimize, leastsq, curve_fit
-from scipy.stats import chi2
+from upAndDown import upAndDown
+#import matplotlib.pyplot as plt
+#import pylab as py
+#from scipy.optimize import minimize, leastsq, curve_fit
+#from scipy.stats import chi2
+
+# Get data
+data  = genfromtxt('../Daten/Kerr Winkel/Si10mal.txt',delimiter=";")
+xData = data.T[0]
+yData = data.T[1]
+
+def correctDataUp(yRealData):
+    data     = genfromtxt('../Daten/Kerr Winkel/Si10mal.txt',delimiter=";")
+    dataUp   = upAndDown(data)[0]
+    yDataUp  = dataUp.T[1]
+    yDataCor = subtract(yRealData,yDataUp)
+    #print yDataUp
+    return yDataCor
+
+print correctDataUp(yData)
